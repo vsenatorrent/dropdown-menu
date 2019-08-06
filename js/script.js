efef10e66b015a0dd0b5ad529c665c63f5e20aa3
vsenatorrent <urpride1@gmail.com>
2019-08-06 16:23:41 +0300

    html refactoring

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
-$(function() {
+// $(function() {
  
-    var menu_ul = $('.menu > li > ul'),
-        menu_a  = $('.menu > li > a');
+//     var menu_ul = $('.menu > li > ul'),
+//         menu_a  = $('.menu > li > a');
      
-    menu_ul.hide();
+//     menu_ul.hide();
  
-    menu_a.click(function(e) {
-        e.preventDefault();
-        if(!$(this).hasClass('active')) {
-            menu_a.removeClass('active');
-            menu_ul.filter(':visible').slideUp('normal');
-            $(this).addClass('active').next().stop(true,true).slideDown('normal');
-        } else {
-            $(this).removeClass('active');
-            $(this).next().stop(true,true).slideUp('normal');
-        }
-    });
+//     menu_a.click(function(e) {
+//         e.preventDefault();
+//         if(!$(this).hasClass('active')) {
+//             menu_a.removeClass('active');
+//             menu_ul.filter(':visible').slideUp('normal');
+//             $(this).addClass('active').next().stop(true,true).slideDown('normal');
+//         } else {
+//             $(this).removeClass('active');
+//             $(this).next().stop(true,true).slideUp('normal');
+//         }
+//     });
  
-});
+// });
